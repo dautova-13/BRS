f3b4047a9e5ad4c795a32e28d4747f6d05737ff5
Luisa <liza.mutalapova@mail.ru>
2024-05-03 19:32:41 +0300

    Сделали миграцию

--- BRS/BRS/settings.py
+++ BRS/BRS/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'studentmarks.apps.StudentmarksConfig'
+    'studentmarks.apps.StudentmarksConfig',
     'corsheaders'
 ]
 
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware', 
-'django.middleware.common.CommonMiddleware'
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'BRS.urls'
@@ -131,4 +131,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL=True
-CORS_ORIGIN_WHITELIST=('http://localhost:8081')
+# CORS_ORIGIN_WHITELIST=('http://localhost:8081')
--- BRS/studentmarks/models.py
+++ BRS/studentmarks/models.py
@@ -11,15 +11,46 @@ class Term(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # Тип.
     type_id = models.UUIDField(primary_key=False, editable=True)
-    
-# Типы периодов.
-class TypeTerm(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # Название.
-    name = models.CharField(max_length=40, blank=False, default='')
     # Дата начала.
     start_data = models.DateField(editable=True)
     # Дата окончания.
     end_data = models.DateField(editable=True)
     # Год.
     year = models.DateField(editable=True)
+
+# Типы периодов.
+class TypeTerm(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Название.
+    name = models.CharField(max_length=40, blank=False, default='')
+
+class Marks(models.Model):
+    # Индификатор.
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Индификатор студента.
+    id_student = models.UUIDField(primary_key=False, editable=True)
+    # Индификатор дисциплины.
+    id_discipline = models.UUIDField(primary_key=False, editable=True)
+    # Значение.
+    value = models.FloatField(primary_key=False, editable=True)
+    # Индификатор типа оценки.
+    mark_type_id = models.UUIDField(primary_key=False, editable=True)
+    # Индификатор семестра.
+    term_id = models.UUIDField(primary_key=False, editable=True)
+
+# Модель тип оценки
+class MarkType(models.Model):
+    # Индификатор.
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Название.
+    name = models.CharField(max_length=40, blank=False, default="")
+    
+class SliceType(models.Model):
+    # Индификатор.
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Название.
+    name = models.CharField(max_length=40, blank=False, default="")
+    # Дата начала.
+    start_data = models.DateField(editable=True)
+    # Дата окончания.
+    end_data = models.DateField(editable=True)
